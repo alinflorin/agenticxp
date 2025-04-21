ac7bb02e140716d670f2a21b8116cfde4bd1fff7
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-22 00:46:44 +0300

    -

---
+++ src/configs/oidc.ts
@@ -0,0 +1,14 @@
+import env from "@/env";
+import { OidcConfiguration } from "@axa-fr/react-oidc";
+
+export const oidcConfig: OidcConfiguration = {
+    client_id: env.VITE_OIDC_CLIENT_ID,
+    redirect_uri: window.location.origin + "/authentication/callback",
+    silent_redirect_uri:
+      window.location.origin + "/authentication/silent-callback",
+    scope: "openid profile email offline_access", // offline_access scope allow your client to retrieve the refresh_token
+    authority: env.VITE_OIDC_ISSUER,
+    service_worker_relative_url: "/OidcServiceWorker.js", // just comment that line to disable service worker mode
+    service_worker_only: false,
+    demonstrating_proof_of_possession: false,
+  };
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,15 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App";
-
-import env from "./env";
-import { OidcProvider, OidcSecure } from "@axa-fr/react-oidc";
-
-const oidcConfig = {
-  client_id: env.VITE_OIDC_CLIENT_ID,
-  redirect_uri: window.location.origin + "/authentication/callback",
-  silent_redirect_uri:
-    window.location.origin + "/authentication/silent-callback",
-  scope: "openid profile email offline_access", // offline_access scope allow your client to retrieve the refresh_token
-  authority: env.VITE_OIDC_ISSUER,
-  service_worker_relative_url: "/OidcServiceWorker.js", // just comment that line to disable service worker mode
-  service_worker_only: false,
-  demonstrating_proof_of_possession: false,
-};
+import { OidcProvider } from "@axa-fr/react-oidc";
+import { Provider } from "./theming/provider";
+import { oidcConfig } from "./configs/oidc";
+import './configs/i18n';
 
 createRoot(document.getElementById("root")!).render(
-  <OidcProvider configuration={oidcConfig}>
-    <OidcSecure>
+  <Provider>
+    <OidcProvider configuration={oidcConfig}>
       <App />
-    </OidcSecure>
-  </OidcProvider>
+    </OidcProvider>
+  </Provider>
 );
