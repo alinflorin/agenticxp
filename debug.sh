d7c7120eb9d75888e6f7095dc72f1b0aaedea432
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-22 00:09:05 +0300

    asd

--- debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-telepresence connect --namespace agenticxp-local || true
-telepresence intercept agenticxp --port 5173:http --env-file .env.local || true
-npm run dev
---
+++ start-telepresence.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+telepresence connect --namespace agenticxp-local || true
+telepresence intercept agenticxp --port 5173:http --env-file .env.local || true
