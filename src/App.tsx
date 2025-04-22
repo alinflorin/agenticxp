7c25126d8ba452ca414b32b81f0b7f3f45174bed
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-22 11:41:03 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Box, Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>React Frontend</h1>
-      <Outlet />
-    </div>
+    <Flex flexDirection="column" w="100%" h="100%">
+      <Header />
+      <Box p={4} flex="auto" minH={0} overflow="auto">
+        <Outlet />
+      </Box>
+      <Footer />
+    </Flex>
   );
 }
