25a3d0b397b0b253c57d909b953fa1e5e4381455
Alin-Florin Cîu <alinflorinciu@icloud.com>
2025-04-29 23:21:55 +0300

    -

--- src/_api/routes/hello.ts
+++ src/_api/routes/hello.ts
@@ -1,10 +1,13 @@
+import axios from "axios";
 import { FastifyInstance, FastifyPluginAsync } from "fastify";
 
 export const helloRoute: FastifyPluginAsync = (
   fastify: FastifyInstance
 ): Promise<void> => {
   fastify.get("/api/hello", {preValidation: fastify.authenticate}, async () => {
-    return { message: "Hello from Fastify!!!" };
+    const opaTest = await axios.post('http://localhost:8081/v1/data/main/authz', {input: {}});
+    console.log(opaTest.data);
+    return { message: "Hello from Fastifyxxx!!!" };
   });
 
   return Promise.resolve();
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,18 @@
 import useToaster from "@/hooks/useToaster"
 import { Button } from "@chakra-ui/react";
+import axios from "axios";
+import { useCallback } from "react";
 
 export default function Home() {
     const showToast = useToaster();
 
+    const testBe = useCallback(async () => {
+        const reply = await axios.get('/api/hello');
+        alert(reply.data);
+    }, []);
+
     return <div>Home<br />
     <Button onClick={() => showToast('test title', 'test message', 'error', {label: 'hey', onClick: () => {alert(1)}})}>Test toast</Button>
+    <Button onClick={testBe}>Test BE</Button>
     </div>
 }
